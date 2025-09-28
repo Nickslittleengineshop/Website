0095edd1f93a6c8d7c30a140f8b203c5713d3683
Nickslittleengineshop <nickslittleengineshop25@gmail.com>
2025-09-28 16:07:03 -0400

    Updated Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -9,10 +9,10 @@ const Footer: React.FC<FooterProps> = ({ handleNavClick }) => {
   return (
     <footer className="bg-black py-8 md:py-12">
       <div className="container mx-auto px-4 md:px-6">
-        <div className="flex flex-col lg:flex-row items-start justify-between space-y-6 lg:space-y-0">
+        <div className="flex flex-col lg:flex-row items-center justify-between space-y-6 lg:space-y-0">
           
           {/* Logo - Left Side - Steel Plate Box */}
-          <div className="flex justify-start">
+          <div className="flex justify-center lg:justify-start">
             <div className="chrome-plate-service relative">
               {/* Corner Screws */}
               <div className="screw-top-left"></div>
@@ -22,7 +22,8 @@ const Footer: React.FC<FooterProps> = ({ handleNavClick }) => {
               
               <div className="relative z-10 p-6 md:p-8">
                 <img 
-                  src="https://raw.githubusercontent.com/Nickslittleengineshop/website-images/refs/heads/main/Nick's%20little%20engine%20shop%20logo.webp" 
+                  src="https://raw.githubusercontent.com/Nickslittleengineshop/website-images/main/Nick's%20little%20engine%20shop%20logo.webp
+" 
                   alt="Nick's Little Engine Shop Logo" 
                   className="h-20 md:h-28 w-auto transition-opacity duration-300 hover:opacity-80"
                 />
@@ -94,8 +95,7 @@ const Footer: React.FC<FooterProps> = ({ handleNavClick }) => {
                     rel="noopener noreferrer"
                     className="bg-blue-600 hover:bg-blue-700 p-3 rounded-full transition-all duration-300 transform hover:scale-110 shadow-lg group"
                   >
-                    <Facebook className="w-5 h-5 md:w-6 md:h-6 text-white group-hover:text-blue-100" aria-hidden="true" />
-                    <span className="sr-only">Visit us on Facebook</span>
+                    <Facebook className="w-5 h-5 md:w-6 md:h-6 text-white group-hover:text-blue-100" />
                   </a>
                 </div>
               </div>
